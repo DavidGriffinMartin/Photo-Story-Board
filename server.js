6e9a95c5bd927dfe14ac962de025175c3066b039
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-06-01 16:49:45 -0400

    wip: Updated code.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
     saveUninitialized: false,
   })
 );
-app.use("/", postsController);
+app.use("/crud", postsController);
 app.use("/users", userController);
 app.use("/sessions", sessionsController);
 
